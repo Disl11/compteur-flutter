7485869e19637ff61e309063949eed0077ade428
Brian Farnier <brian.pro1993@gmail.com>
2025-11-18 11:06:42 +0100

    fix list en dur + layout

--- lib/pageTeams/teamA.dart
+++ lib/pageTeams/teamA.dart
@@ -1,22 +1,40 @@
 import 'package:flutter/material.dart';
 
-class TeamA extends StatefulWidget {
-  final List joueursTeamA = [
+class TeamA extends StatelessWidget {
+  final List playersTeamA = [
     {"Name": "Ludovic", "LastName": "Kader"},
+    {"Name": "Jordan", "LastName": "Michael"},
   ];
   TeamA({super.key});
 
   @override
-  State<TeamA> createState() => _TeamAState();
-}
-
-class _TeamAState extends State<TeamA> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text("Team A")),
+        title: Center(child: Text("Players Team A")),
         backgroundColor: Colors.orangeAccent,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Center(
+            child: SizedBox(
+              height: 672,
+              width: 250,
+              child: ListView.builder(
+                itemCount: playersTeamA.length,
+                itemBuilder: (Context, index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text("Nom : ${playersTeamA[index]["LastName"]}"),
+                      subtitle: Text("Prenom : ${playersTeamA[index]["Name"]}"),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
